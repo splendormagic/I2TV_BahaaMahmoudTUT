23a68d6e67495d3a86a35d6192cece416c503627
splendormagic <splendor.magic@gmail.com>
2024-04-17 17:07:14 +0200

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,4 +1,4 @@
-from IPython.display import HTML, Audio, clear_output
+from IPython.display import HTML, Audio, clear_output, Image
 from google.colab.output import eval_js
 from base64 import b64decode, b64encode
 import numpy as np
